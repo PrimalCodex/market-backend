b5ad2f94aaf828cd9ac540664957de67844c7f0d
PrimalCodex <nkevro31@gmail.com>
2024-07-31 14:16:25 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors');
+
+const {initializePostgres} = require("./db");
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('disi bekend')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+app.post('/init', async (req, res) => {
+    try {
+        await initializePostgres()
+        return res.status(200).json({})
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json(error)
+    }
+});
+
+
+require('/src/api/authApi')(app)
---
+++ src/db.js
@@ -0,0 +1,26 @@
+// db.js
+const postgres = require('postgres')
+
+const sql = postgres({
+    host: 'localhost',            // Postgres ip address[s] or domain name[s]
+    port: 5432,          // Postgres server port[s]
+    database: 'market',            // Name of database to connect to
+    username: 'postgres',            // Username of database user
+    password: 'postgres',            // Password of database user
+})
+
+const initializePostgres = async () => {
+    await sql`
+    DROP TABLE IF EXISTS users
+  `
+    await sql`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      email varchar(100) NOT NULL UNIQUE,
+      password varchar(450) NOT NULL,
+      role varchar(20) NOT NULL
+    )
+   `
+}
+
+module.exports = {sql, initializePostgres}
